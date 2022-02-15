7e6b4804dfd0393e08b44d3e9ddc3e89a7836478
undanganmanten <agungtprass@gmail.com>
2022-02-15 18:46:58 +0700

    sheilla ridwan

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+function getParameterByName(name, url = window.location.href) {
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+      results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+// document.title = getParameterByName('to');
+document.querySelector('#to').innerHTML = getParameterByName('to');
+
 $(window).on("load",function(){
   $(".loader-wrapper").fadeOut("slow");
 });
